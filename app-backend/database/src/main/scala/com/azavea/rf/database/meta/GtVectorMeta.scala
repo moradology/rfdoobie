d88a720c5b437e5d98e262fca4893f1e5cd03600
Nathan Zimmerman <npzimmerman@gmail.com>
2018-01-22 18:19:47 -0500

    Fix name of gtvector meta trait

--- app-backend/database/src/main/scala/com/azavea/rf/database/meta/GtVectorMeta.scala
+++ app-backend/database/src/main/scala/com/azavea/rf/database/meta/GtVectorMeta.scala
@@ -1,11 +1,11 @@
-package com.azavea.rf.database
+package com.azavea.rf.database.meta
 
 import com.azavea.rf.datamodel.AOI
 
 import doobie._, doobie.implicits._
 import cats._, cats.data._, cats.effect.IO
 import cats.syntax.either._
-import doobie.postgres._, doobie.postgres.implicits._//, doobie.postgres.pgisimplicits._
+import doobie.postgres._, doobie.postgres.implicits._, doobie.postgres.pgisimplicits._
 import doobie.util.invariant.InvalidObjectMapping
 import org.postgis.jts._
 import com.vividsolutions.jts.geom
@@ -13,7 +13,9 @@ import io.circe._
 import doobie.scalatest.imports._
 import org.scalatest._
 import org.postgresql.util.PGobject
+import org.{postgis => pg}
 import geotrellis.vector._
+import geotrellis.slick.Projected
 
 import scala.reflect.runtime.universe.TypeTag
 import scala.reflect.ClassTag
@@ -21,31 +23,93 @@ import scala.reflect.ClassTag
 
 trait GtVectorMeta {
 
-  private val jtsGeometryType = Meta.other[JtsGeometry]("geometry")
+  private def gtPoint2pgPoint(gtPoint: Point): pg.Point =
+    new pg.Point(gtPoint.x, gtPoint.y)
 
-  // Constructor for geometry types via the `Geometry` member of JTSgeometry
-  private def geometryType[A >: Null <: geom.Geometry: TypeTag, B >: Null <: Geometry: TypeTag](wrapperF: A => B)(implicit A: ClassTag[A]): Meta[B] =
-    jtsGeometryType.xmap[B](
-      { g =>
-        try {
-          val jtsGeom = A.runtimeClass.cast(g.getGeometry).asInstanceOf[A]
-          wrapperF(jtsGeom)
-        } catch {
-          case _: ClassCastException => throw InvalidObjectMapping(A.runtimeClass, g.getGeometry.getClass)
-        }
-      },
-      { gtGeom: Geometry => new JtsGeometry(gtGeom.jtsGeom) }
-    )
+  private def gtLine2pgLine(gtLine: Line): pg.LineString = {
+    val pointArr = gtLine.points.map(gtPoint2pgPoint)
+    new pg.LineString(pointArr)
+  }
 
-  // PostGIS Geometry Types
-  implicit val GeometryType           = geometryType[geom.Geometry, Geometry](Geometry.apply _)
-  implicit val GeometryCollectionType = geometryType[geom.GeometryCollection, GeometryCollection](GeometryCollection.apply _)
-  implicit val MultiLineStringType    = geometryType[geom.MultiLineString, MultiLine](MultiLine.apply _)
-  implicit val MultiPolygonType       = geometryType[geom.MultiPolygon, MultiPolygon](MultiPolygon.apply _)
-  implicit val LineStringType         = geometryType[geom.LineString, Line](Line.apply _)
-  implicit val MultiPointType         = geometryType[geom.MultiPoint, MultiPoint](MultiPoint.apply _)
-  implicit val PolygonType            = geometryType[geom.Polygon, Polygon](Polygon.apply _)
-  implicit val PointType              = geometryType[geom.Point, Point](Point.apply _)
+  private def gtPoly2pgPoly(gtPoly: Polygon): pg.Polygon = {
+    val exterior = new pg.LinearRing(gtPoly.exterior.points.map(gtPoint2pgPoint))
+    val holes: Array[pg.LinearRing] = gtPoly.holes.map({ hole =>
+      new pg.LinearRing(hole.points.map(gtPoint2pgPoint))
+    })
+    new pg.Polygon(holes :+ exterior)
+  }
+
+  private def pgPoly2gtPoly(pgPoly: pg.Polygon): Polygon = {
+    val rings = for {
+      ring <- 0 to (pgPoly.numGeoms - 1)
+    } yield {
+      pgPoly.getSubGeometry(ring).asInstanceOf[pg.LinearRing]
+    }
+
+    val lines = rings.map({ ring =>
+      val ringPoints = ring.getPoints.map({ pnt => Point(pnt.x, pnt.y) })
+      Line(ringPoints)
+    })
+
+    Polygon(lines.head, lines.tail)
+  }
+
+  implicit val pointMeta = Meta[pg.Point].xmap[Projected[Point]](
+    pgPoint => {
+      Projected[Point](
+        Point(pgPoint.x, pgPoint.y),
+        pgPoint.getSrid
+      )
+    },
+    gtPoint => {
+      val point = gtPoint2pgPoint(gtPoint.geom)
+      point.setSrid(gtPoint.srid)
+      point
+    }
+  )
+
+  implicit val lineMeta = Meta[pg.LineString].xmap[Projected[Line]](
+    pgLine => {
+      Projected[Line](
+        Line(pgLine.getPoints.map({ pnt => Point(pnt.x, pnt.y) })),
+        pgLine.getSrid
+      )
+    },
+    gtLine => {
+      val line = gtLine2pgLine(gtLine)
+      line.setSrid(gtLine.srid)
+      line
+    }
+  )
+
+  implicit val polygonMeta = Meta[pg.Polygon].xmap[Projected[Polygon]](
+    pgPoly => {
+      Projected[Polygon](
+        pgPoly2gtPoly(pgPoly),
+        pgPoly.getSrid
+      )
+    },
+    gtPoly => {
+      val poly = gtPoly2pgPoly(gtPoly)
+      poly.setSrid(gtPoly.srid)
+      poly
+    }
+  )
+
+  implicit val multipolygonMeta = Meta[pg.MultiPolygon].xmap[Projected[MultiPolygon]](
+    pgMPoly => {
+      val polygons = pgMPoly.getPolygons.map(pgPoly2gtPoly)
+      Projected[MultiPolygon](
+        MultiPolygon(polygons),
+        pgMPoly.getSrid
+      )
+    },
+    gtMPoly => {
+      val multipoly = new pg.MultiPolygon(gtMPoly.polygons.map(gtPoly2pgPoly))
+      multipoly.setSrid(gtMPoly.srid)
+      multipoly
+    }
+  )
 
 }
 
--- app-backend/database/src/main/scala/com/azavea/rf/database/meta/package.scala
+++ app-backend/database/src/main/scala/com/azavea/rf/database/meta/package.scala
@@ -2,7 +2,7 @@ package com.azavea.rf.database
 
 
 package object meta {
-  trait RFMeta extends JtsMeta
+  trait RFMeta extends GtVectorMeta
       with CirceJsonbMeta
       with VisibilityEnumMeta
       with ExportStatusEnumMeta
